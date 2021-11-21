e004c80a563ae2d703a83548440d276527b6725f
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-11-20 19:52:46 -0500

    added part1 state hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
-const App = () => (
-	<div>
-		<p> hola mindo </p>
-	</div>
+import React , { useState } from "react";
+
+const Display = ({counter}) => (
+		<h1>{counter}</h1>
 );
+
+const Button = ({onClick, text}) => (
+				<button  onClick={onClick} > {text} </button>	
+);
+
+const App = () => {
+
+	const [counter , setCounter] = useState(0);
+
+	const increaseByOne = () => setCounter(counter + 1);
+	const setToZero = () => setCounter(0);
+	const decreaseByOne = () => setCounter(counter - 1);
+
+
+	return (
+			<>	
+				<Display counter =  {counter} />
+				<Button  onClick={increaseByOne}  text={"+"} />	
+				<Button  onClick={setToZero}  text={"Zero"} />	
+				<Button  onClick={decreaseByOne}  text={"-"} />				
+			</>			
+	)	
+};
 
 export default App;
