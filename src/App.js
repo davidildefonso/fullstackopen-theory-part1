038418ca186e61817443758e93de6acfd627e672
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-11-20 20:29:48 -0500

    end of theory part 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import React , { useState } from "react";
 
-const Display = ({counter}) => (
-		<h1>{counter}</h1>
-);
 
-const Button = ({onClick, text}) => (
-				<button  onClick={onClick} > {text} </button>	
-);
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 
 const App = () => {
+  const [value, setValue] = useState(10)
 
-	const [counter , setCounter] = useState(0);
+  const setToValue = (newValue) => {
+    setValue(newValue)
+  }
 
-	const increaseByOne = () => setCounter(counter + 1);
-	const setToZero = () => setCounter(0);
-	const decreaseByOne = () => setCounter(counter - 1);
-
-
-	return (
-			<>	
-				<Display counter =  {counter} />
-				<Button  onClick={increaseByOne}  text={"+"} />	
-				<Button  onClick={setToZero}  text={"Zero"} />	
-				<Button  onClick={decreaseByOne}  text={"-"} />				
-			</>			
-	)	
-};
-
+  return (
+    <div>
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
+    </div>
+  )
+}
 export default App;
